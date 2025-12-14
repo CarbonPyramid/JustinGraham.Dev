c755de0e75c7f99839ff3fe11a86168eb4349abf
CarbonPyramid <CarbonPyramid@users.noreply.github.com>
2025-12-14 15:15:33 -0600

    Update: Modified 2 file(s)

--- pages/About/index.tsx
+++ pages/About/index.tsx
@@ -16,7 +16,7 @@ const About = () => {
           </Grid>
           <Grid item>
             <Typography variant="body1">
-              With technology being a hobby of mine since I was a small child of the 80's, technology means a lot of different things to me. I've been interested in software development since being exposed to it via MySpace. Thanks Tom, you were never in my top 8 but will always be in my top 8. I've got a wide array technology software and technology hardware build, deployment, and support experience that spans small, medium, and large scale techonology infrastructure. The languages necessary to cross these platforms is my focus, automation for basic function, training AI models for enhanced capabilities, focusing on a user driven experience.
+              With technology being a hobby of mine since I was a small child of the 80's, technology means a lot of different things to me. I've been interested in software development since being exposed to it via MySpace. Thanks Tom, you were never in my top 8 but will always be in my top 8. I've got a wide array of technology software & technology hardware build, deployment & support experience that spans small, medium & large scale techonology infrastructure. My syntactical focus is on interoperability, automation for basic function, dev ops for managing high level operation, training AI models for enhanced capabilities, securing all layers & prioritizing a user driven experience.
             </Typography>
           </Grid>
         </Grid>
